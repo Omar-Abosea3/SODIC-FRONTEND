34f78066e9de3dce3a4ea931bce87a5036e768db
Omar-Abosea3 <oa615010@gmail.com>
2025-05-25 16:17:41 +0300

    edits

--- src/componants/Layout/Layout.jsx
+++ src/componants/Layout/Layout.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import  { useEffect } from 'react'
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Foooter/Footer';
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 export default function Layout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return <>
   <Navbar/>
   <Outlet/>
